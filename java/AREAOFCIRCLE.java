f5430c350072b3572060e0b1c6efb081c44311ac
urgent15 <156510584+urgent15@users.noreply.github.com>
2024-01-17 14:39:33 +0000

    Create AREAOFCIRCLE.java

---
+++ java/AREAOFCIRCLE.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class areaofcircle {
+    public static void main(String[] args) {
+        int r; double s,pi=3.14;
+        System.out.println("Enter the radius of the circle");
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextInt();
+        s = pi*r*r;
+        p=2*pi*r;
+        System.out.println("Area of the circle is "+s);
+        System.out.println("Perimeter of the circle is "+p);
+    }
+}
