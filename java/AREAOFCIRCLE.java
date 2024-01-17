d69e14fcfceac4dd68099aaa51d8aa2d317d553b
urgent15 <156510584+urgent15@users.noreply.github.com>
2024-01-17 15:19:51 +0000

    Update AREAOFCIRCLE.java

--- java/AREAOFCIRCLE.java
+++ java/AREAOFCIRCLE.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class areaofcircle {
     public static void main(String[] args) {
-        int r; double s,pi=3.14;
+        int r; double p,s,pi=3.14;
         System.out.println("Enter the radius of the circle");
         Scanner sc = new Scanner(System.in);
         r = sc.nextInt();
