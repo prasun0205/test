7585ba9f3b01d253cde27745b2a78e6fba5b425c
urgent15 <156510584+urgent15@users.noreply.github.com>
2024-01-17 16:13:21 +0000

    Create CLASS-MECHANISM.java

---
+++ java/CLASS-MECHANISM.java
@@ -0,0 +1,23 @@
+public class Car {
+    String make;
+    String model;
+    int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Car Information:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022);
+        myCar.displayInfo();
+    }
+}
