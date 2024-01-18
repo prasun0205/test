fbde53cd80c2c13aebcc9d9e258c5ff83fefaa8d
urgent15 <156510584+urgent15@users.noreply.github.com>
2024-01-18 14:51:27 +0000

    Update MULTITHREAD&5MS.java

--- java/MULTITHREAD&5MS.java
+++ java/MULTITHREAD&5MS.java
@@ -1,1 +1,20 @@
-
+class MyRunnable implements Runnable {
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " is running.");
+            Thread.sleep(500);
+            System.out.println(Thread.currentThread().getName() + " has completed.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+public class ThreadExample {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyRunnable(), "Thread-1");
+        Thread thread2 = new Thread(new MyRunnable(), "Thread-2");
+        thread1.start();
+        thread2.start();
+        System.out.println("Main thread continues.");
+    }
+}
