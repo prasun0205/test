e14ee6f52177d016c367b8f6ce384e5532464adb
urgent15 <156510584+urgent15@users.noreply.github.com>
2024-01-18 22:45:52 +0530

    Create ODD-EVEN.sh

---
+++ os/ODD-EVEN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Enter a number: "
+read number
+
+is_odd_or_even() {
+  local num=$1
+
+  if [ $((num % 2)) -eq 0 ]; then
+    echo "$num is an even number."
+  else
+    echo "$num is an odd number."
+  fi
+}
+
+is_odd_or_even $number
