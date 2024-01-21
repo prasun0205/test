1aa6aabe6971a5b3ed9622c16243035a1f7654f2
urgent15 <156510584+urgent15@users.noreply.github.com>
2024-01-21 21:32:09 +0530

    Update NWETON-FORWARD.C

--- nm/NWETON-FORWARD.C
+++ nm/NWETON-FORWARD.C
@@ -1,58 +1,64 @@
+//newton forward interpolation
 #include<stdio.h>
 #include<math.h>
 int main()
 {
-	float x[15],y[15],X,p=1,s,h,d;  
-	int i,j,n;
-	printf("\t\t Output\n");
-	printf("\n Enter the number of points: ");
-	scanf("%d",&n);
-	printf("\n Enter the value of X at which y required ");
-	scanf("%f",&X);
-	printf("\n Enter the value of x & y:\n\n");
-	printf("\n x \t\t y=f(x)\n");
-	for(i=1;i<=n;i++)
-	scanf("%f%f",& x[i],&y[i]);
-	h=x[2]-x[1];
-	printf("\n h= %f\n",h);
-	s=(X-x[1])/h;
-	d=y[1];
-	printf("s=%f,\t d=%f",s,d);
-	for(i=1;i<=n-1;i++)
-	{
-		for(j=1;j<=n-1;j++)
-		{
-			y[j]=y[j+1]-y[j];
-			printf("\n%f\n",y[j]);
-		}
-		p=p*(s-i+1)/i;
-		d=d+p*y[1];
-	}
-	printf("\n\n The result is: \n\n");
-    printf("y(%1.2f)=%3.5f",X,d);
-    return 0;
+float x[10],y[10],X,p=1,u,h,d;
+int i,j,n;
+printf("\t\t Output\n");
+printf("\n Enter the no.of points:");
+scanf("%d",&n);
+printf("\n Enter the value of X at which y is reqd.");
+scanf("%f",&X);
+printf("\n Enter the values of x and y: \n\n");
+printf("x\t\ty=f(x)\n");
+for(i=1;i<=n;i++)
+scanf("%f\t%f",&x[i],&y[i]);
+h=x[2]-x[1];
+printf("\n h=%f\n",h);
+u=(X-x[1])/h;
+d=y[1];
+printf("u=%f,\td=%f",u,d);
+for(i=1;i<=n-1;i++)
+{
+for(j=1;j<=n-1;j++)
+{
+y[j]=y[j+1]-y[j];
+printf("\n%f\n",y[j]);
+}
+p=p*(u-i+1)/i;
+d=d+p*y[1];
+printf("\n%f",d);
+
+}
+printf("\n\nThe result of Newton Forward Interpolation\n\n");
+printf("y(%f)=%f",X,d);
 }
 
-//OUTUT
-
-                 Output
-
- Enter the number of points: 5
-
- Enter the value of X at which y required 2.1
-
- Enter the value of x & y:
 
 
- x               y=f(x)
-2               16.56
-2.2             21.78
-2.4             32.56
-2.6             41.97
-2.8             52.68
 
- h= 0.200000
-s=0.499999,      d=16.559999
+
+//OUTPUT:
+
+
+
+
+/tmp/57SCx3IIgt.o
+Output
+
+ Enter the no.of points:5
+ Enter the value of X at which y is reqd.2.1
+ Enter the values of x and y: 
+
+x		y=f(x)
+2       16.56
+2.2     21.78
+2.4     32.56
+2.6     41.97
+2.8     52.68
+h=0.200000
+u=0.499999,	d=16.559999
 5.220001
 
 10.780001
@@ -61,6 +67,7 @@ s=0.499999,      d=16.559999
 
 10.709999
 
+19.169996
 5.559999
 
 -1.370001
@@ -69,6 +76,7 @@ s=0.499999,      d=16.559999
 
 41.970001
 
+18.474997
 -6.930000
 
 2.670000
@@ -77,6 +85,7 @@ s=0.499999,      d=16.559999
 
 10.709999
 
+18.041872
 9.600000
 
 38.000000
@@ -85,7 +94,8 @@ s=0.499999,      d=16.559999
 
 41.970001
 
+17.666872
 
- The result is: 
+The result of Newton Forward Interpolation
 
-y(2.10)=17.66687
+y(2.100000)=17.666872
